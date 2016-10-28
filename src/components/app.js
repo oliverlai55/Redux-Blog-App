ff9c272f1912ffaa8e539014286d44fc4d105ee0
oliverlai55 <oliverlai55@gmail.com>
2016-10-27 22:16:49 -0400

    Add IndexRoute component to render during / directory

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        {this.props.children}
+      </div>
     );
   }
 }
+  //this.props.children. If we nest child component inside parent component and want the parent to render the chil component, then need to use this.props.children in the parent compoennt
+  // this.props.children is where the child components will take place on the page
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
+
+const Greeting = () => {
+  return <div>Hey There</div>
+};
+
 
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <IndexRoute component={PostsIndex} />
+    <Route path="greet" component={Greeting} />
+    <Route path="greet2" component={Greeting} />
+    <Route path="greet3" component={Greeting} />
+  </Route>
 );
+
+// Different routes /, /greet, /greet2..etc
+//App is the root container
+// IndexRoute is a helper like route, for /, it will also show IndexRoute
