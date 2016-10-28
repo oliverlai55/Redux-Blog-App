c86c04936130947b6df6518de2945b76aa006d47
oliverlai55 <oliverlai55@gmail.com>
2016-10-27 21:47:39 -0400

    Link routes to home directory /

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.container'));
+
+// browserHistory is a object that tells react-router how to make changes based on URL.  Whenever the URL updates, react-router is going to interpret after the protocol
+// There is also hashHistory, which keeps track of urls wnding with # and make changes based on anything after #
+// history takes url and passes to react router
