064046f5d1a219f1e96392a84d22b75fcc2fea75
oliverlai55 <oliverlai55@gmail.com>
2016-10-30 02:33:23 -0400

    Succinct fetchData dispatch method

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,14 +1,16 @@
 import { FETCH_POSTS } from '../actions/index';
 
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = { all: [], post: null};
 
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
   case FETCH_POSTS:
-
+    return { ...state, all: action.payload.data };
   default:
       return state;
   }
 }
+
+  // FETCH_POSTS action return our application state, ... takes all current state, and add 'all' action.payload.data
