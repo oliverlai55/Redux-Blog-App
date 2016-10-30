f418c6da527397f9944d80dceeb02d2689988535
oliverlai55 <oliverlai55@gmail.com>
2016-10-30 03:08:56 -0400

    Setup form in component side

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>Create Form</div>
+      <form>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+            <label>Title</label>
+            <input type='text' className="form-control" />
+        </div>
+
+        <div className="form-group">
+            <label>Categories</label>
+            <input type='text' className="form-control" />
+        </div>
+
+        <div className="form-group">
+            <label>Content</label>
+            <textarea className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
+
+//reduxForm is almost identical to the connect functino from react-redux library
+
+//user types something in  ... record it on application state
+// state === {
+//   form: {
+//     PostsNewForm: {
+//       title: '....',
+//       categories: '....'.
+//     }
+//   }
+// }
