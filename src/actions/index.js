a6da624d27765af1161cb4c6a502e1165b10ecba
oliverlai55 <oliverlai55@gmail.com>
2016-10-30 02:32:12 -0400

    Correct typo error

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,10 +9,10 @@ const API_KEY = '?key=asdfghj';
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
 
-  return (
+  return {
     type: FETCH_POSTS,
     payload: request
-  );
+  };
 }
 
 //This is where we request the blog data from backend using Axios
