67cdbf8aa8acf9fe04a6588588972d054830b9cf
oliverlai55 <oliverlai55@gmail.com>
2016-10-30 02:32:29 -0400

    Correct duplicate import reducer

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reducers from './reducers';
 import routes from './routes';
 import promise from 'redux-promise';
 
-import reducers from './reducers';
+// import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(
   promise
