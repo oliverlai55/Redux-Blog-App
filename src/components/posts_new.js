52461f21dfde374f5e97c084adb7ce22efc962de
oliverlai55 <oliverlai55@gmail.com>
2016-11-01 00:35:16 -0400

    Add PropTypes from react-router

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+    //tells react that we want access to the property from parent component using the context property
+  }
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // blog post has been created, navigate the user to the Index
+        //navigate by calling this.context.router.push with the new path to navigate to
+        this.context.router.push('/');
+      });
+  }
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
     //const handleSubmit = this.props.handleSubmit
     //const title = this.props.fields.title
 
-    ;
+
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create A New Post</h3>
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
             <label>Title</label>
