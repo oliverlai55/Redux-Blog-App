d4927c590cea2b0151bfd9d558917eb2a1607fcd
oliverlai55 <oliverlai55@gmail.com>
2016-11-07 01:00:06 -0500

    Set up ability to show post id and render on page

--- src/routes.js
+++ src/routes.js
@@ -4,17 +4,23 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
-const Greeting = () => {
-  return <div>Hey There</div>
-};
+import PostsShow from './components/posts_show';
+
+// const Greeting = () => {
+//   return <div>Hey There</div>
+// };
 
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
+
+//this.props.params.id
+// pull the id out of each post
 
 // Different routes /, /greet, /greet2..etc
 //App is the root container
